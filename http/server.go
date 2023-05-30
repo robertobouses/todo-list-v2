142ed741b5b98dd24c201c067575f452bbadf61d
Roberto Fernandez Toro <robertobouses@gmail.com>
2023-05-30 17:02:45 +0200

    handler

--- http/server.go
+++ http/server.go
@@ -1,17 +1,36 @@
 package http
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Server struct {
-	engine *gin.Engine
+	engine  *gin.Engine
+	handler Handler
+}
+
+type Handler struct {
 }
 
 func NewServer() Server {
+	router := gin.Default()
+	handler := Handler{}
+
+	router.GET("/hello", handler.Hello)
+
 	return Server{
-		engine: gin.Default(),
+		engine:  router,
+		handler: handler,
 	}
+}
+
+func (H Handler) Hello(c *gin.Context) {
+	c.String(http.StatusOK, "Hello")
 }
 
 func (s *Server) Run(puerto string) error {
 	return s.engine.Run(puerto)
+
 }
