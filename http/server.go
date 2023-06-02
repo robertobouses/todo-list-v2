55b8243533e98ff7d2395e4e77695358bd1ceb12
Roberto Fernandez Toro <robertobouses@gmail.com>
2023-06-02 12:44:54 +0200

    CreateUser

--- http/server.go
+++ http/server.go
@@ -14,12 +14,17 @@ type Server struct {
 type Handler struct {
 }
 
+type reg struct {
+	Email    string `json: "email"`
+	Password string `json: "password"`
+}
+
 func NewServer() Server {
 	router := gin.Default()
 	handler := Handler{}
 
 	router.GET("/hello", handler.Hello)
-
+	router.POST("/user", handler.CreateUser)
 	return Server{
 		engine:  router,
 		handler: handler,
@@ -33,4 +38,12 @@ func (H Handler) Hello(c *gin.Context) {
 func (s *Server) Run(puerto string) error {
 	return s.engine.Run(puerto)
 
+}
+
+func (H Handler) CreateUser(c *gin.Context) {
+	var request reg
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 }
